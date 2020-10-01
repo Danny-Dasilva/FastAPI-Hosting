a07858fbbcfc40f23af648e28faa07ca51209a40
Danny Dasilva <yahchayildasilva@gmail.com>
2020-09-30 20:09:14 -0400

    Update encode_h264.sh

--- scripts/encode_h264.sh
+++ scripts/encode_h264.sh
@@ -1,1 +1,1 @@
-ffmpeg -y -i sourceFile -r 30000/1001 -b:a 2M -bt 4M -vcodec libx264 -pass 1 -coder 0 -bf 0 -flags -loop -wpredp 0 -an targetFile.mp4
+ffmpeg -y -i in.mp4 -r 30000/1001 -b:a 2M -bt 4M -vcodec libx264 -pass 1 -coder 0 -bf 0 -flags -loop -wpredp 0 -an targetFile.mp4
